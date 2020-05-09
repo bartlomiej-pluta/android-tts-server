84449b573ea49733fa05af936dfa95099fb9e5b3
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2020-05-09 15:29:23 +0200

    Allow to change server port only when server is down

--- app/src/main/java/io/bartek/preference/PreferencesFragment.kt
+++ app/src/main/java/io/bartek/preference/PreferencesFragment.kt
@@ -1,18 +1,53 @@
 package io.bartek.preference
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.text.InputType
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import io.bartek.R
+import io.bartek.service.ForegroundService
+import io.bartek.service.ServiceState
 
 class PreferencesFragment : PreferenceFragmentCompat() {
     private lateinit var portPreference: IntEditTextPreference
     private lateinit var sayEndpointPreference: SwitchPreference
     private lateinit var waveEndpointPreference: SwitchPreference
     private lateinit var ttsEnginePreference: Preference
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.let {
+                updateViewAccordingToServiceState(
+                    ServiceState.valueOf(it.getStringExtra("STATE") ?: "STOPPED")
+                )
+            }
+        }
+    }
+
+    private fun updateViewAccordingToServiceState(state: ServiceState) {
+        portPreference.isEnabled = state == ServiceState.STOPPED
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager
+            .getInstance(context!!)
+            .registerReceiver(receiver, IntentFilter("io.bartek.web.server.CHANGE_STATE"))
+        updateViewAccordingToServiceState(ForegroundService.state)
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager
+            .getInstance(context!!)
+            .unregisterReceiver(receiver)
+        super.onPause()
+    }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
@@ -25,5 +60,6 @@ class PreferencesFragment : PreferenceFragmentCompat() {
             startActivity(Intent("com.android.settings.TTS_SETTINGS"))
             true
         }
+        updateViewAccordingToServiceState(ForegroundService.state)
     }
 }
